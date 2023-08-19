ae27e3b588b65609c1a037dc7c42cb1737d57b1d
Bimalap <bimalapaneru246@gmail.com>
2023-08-19 17:26:11 +0545

    added upto fee

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,9 +3,16 @@
 
 <div class="content-wrapper">
     <div class="container-full">
+<br><br>
+
 
     <p style="text-align: center">Advanced school system</p>
     </div>
+    <br><br>
+<br><br><br>
+
+
+<h3 style="text-align: center">DASHBOARD COMMING SOON</h3>
 </div>
 
 
