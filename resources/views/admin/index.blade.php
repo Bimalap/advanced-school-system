0a6fd309ff46f250bb56da357f232bda9bf6c27a
Bimalap <bimalapaneru246@gmail.com>
2023-08-19 12:27:36 +0545

    Complted 3 module

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,12 @@
+@extends('admin.admin_master')
+@section('admin')
+
+<div class="content-wrapper">
+    <div class="container-full">
+
+    <p style="text-align: center">Advanced school system</p>
+    </div>
+</div>
+
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Backend\UserController;
+use App\Http\Controllers\Backend\ProfileController;
+
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.index');
+    })->name('dashboard'); 
+});
+
+route::get('/admin/logout',[AdminController::class,'logout'])->name('admin.logout');
+
+//user Manage Route
+Route::prefix('users')->group(function(){
+
+    Route::get('/view', [UserController::class, 'UserView'])->name('user.view');
+
+    Route::get('/add', [UserController::class, 'UserAdd'])->name('user.add');
+    Route::post('/store', [UserController::class, 'UserStore'])->name('users.store');
+
+    Route::get('/edit/{id}', [UserController::class, 'UserEdit'])->name('users.edit');
+    Route::post('/update/{id}', [UserController::class, 'UserUpdate'])->name('users.update');
+    Route::get('/delete/{id}', [UserController::class, 'UserDelete'])->name('users.delete');
+    }); 
+
+    //profile management
+Route::prefix('profile')->group(function(){
+
+    Route::get('/view', [ProfileController::class, 'ProfileView'])->name('profile.view');
+    Route::get('/edit', [ProfileController::class, 'ProfileEdit'])->name('profile.edit');
+    Route::Post('/store', [ProfileController::class, 'ProfileStore'])->name('profile.store');
+
+    Route::get('/password/view', [ProfileController::class, 'PasswordView'])->name('password.view');
+    Route::post('/password/update', [ProfileController::class, 'PasswordUpdate'])->name('password.update');
+
+
+     });
