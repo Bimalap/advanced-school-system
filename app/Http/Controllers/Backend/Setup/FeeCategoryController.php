e6fb9be0a47f3385b07556c1f81eb1632fc9cef1
Bimalap <bimalapaneru246@gmail.com>
2023-08-19 17:31:34 +0545

    Added fee catogry

---
+++ app/Http/Controllers/Backend/Setup/FeeCategoryController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Setup;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\FeeCategory; 
+
+
+class FeeCategoryController extends Controller
+{
+    //view fee
+    public function ViewFeeCat(){
+    	$data['allData'] = FeeCategory::all();
+    	return view('backend.setup.fee_category.view_fee_cat',$data);
+    }
+
+    //add fee category
+
+    public function FeeCatAdd(){
+    	return view('backend.setup.fee_category.add_fee_cat');
+    }
+    public function FeeCatStore(Request $request){
+
+        $validatedData = $request->validate([
+            'name' => 'required|unique:fee_categories,name',
+            
+        ]);
+
+        $data = new FeeCategory();
+        $data->name = $request->name;
+        $data->save();
+
+        $notification = array(
+            'message' => 'Fee Category Inserted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('fee.category.view')->with($notification);
+    }
+    //edit fee category
+    public function FeeCatEdit($id){
+        $editData = FeeCategory::find($id);
+        return view('backend.setup.fee_category.edit_fee_cat',compact('editData'));
+    }
+    public function FeeCategoryUpdate(Request $request,$id){
+
+        $data = FeeCategory::find($id);
+        
+        $validatedData = $request->validate([
+               'name' => 'required|unique:fee_categories,name,'.$data->id
+               
+           ]);
+   
+           
+           $data->name = $request->name;
+           $data->save();
+   
+           $notification = array(
+               'message' => 'Fee Category Updated Successfully',
+               'alert-type' => 'success'
+           );
+   
+           return redirect()->route('fee.category.view')->with($notification);
+       }
+
+       //fee category delete 
+       public function FeeCategoryDelete($id){
+        $user = FeeCategory::find($id);
+        $user->delete();
+
+        $notification = array(
+            'message' => 'Fee Category Deleted Successfully',
+            'alert-type' => 'info'
+        );
+
+        return redirect()->route('fee.category.view')->with($notification);
+
+    }
+
+}
